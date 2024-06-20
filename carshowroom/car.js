1154a6e3afc8c77b94075bcd6b7a5d5971c9b3de
Dev2vinod <vinodsinghfun@gmail.com>
2024-06-20 23:56:28 +0530

    pura hua kam

---
+++ carshowroom/car.js
@@ -0,0 +1,435 @@
+
+
+var carlist = {
+    suzuki : {
+        mehran : {
+            model:'2020',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Manual',
+            Mileage: "14 to 18 KM/LITER",
+            Wheel :	'12 in',
+            price: '1,700,000 PKR',
+            color: ['red','white','silver'] ,
+            image :'./mehran.jpg'  
+        },
+        alto :{
+            model:'2022',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Manual',
+            Mileage: "17 to 22 KM/LITER",
+            Wheel :	'14 in',
+            price: '2,400,000 PKR',
+            color: ['red','white','black'] ,
+            image :'./alto.webp'    
+  
+        
+        },
+        cultus :{
+            model:'2021',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "15 to 19 KM/LITER",
+            Wheel :	'14 in',
+            price: '2,900,000 PKR',  
+            color: ['blue','white','silver']  ,
+            image :'./cultus.jpg'   
+
+        
+        },
+        swift :{
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "15 to 19 KM/LITER",
+            Wheel :	'14 in',
+            price: '4,200,000 PKR',  
+            color: ['red','white','black']  ,
+            image :'./swift.webp'   
+
+
+        },
+        vegonR : {
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "15 to 19 KM/LITER",
+            Wheel :	'14 in',
+            price: '3,200,000 PKR',  
+            color: ['red','white','black'],
+            image :'./wagnor.webp'     
+
+
+        }
+        
+    },
+    honda :{
+        city : {
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Automatic',
+            Mileage: "15 to 19 KM/LITER",
+            Wheel :	'14 in',
+            price: '4,200,000 PKR',  
+            color: ['red','white','black'],
+            image :'./mehran.jpg'    
+        },
+        BRV :{
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "15 to 19 KM/LITER",
+            Wheel :	'14 in',
+            price: '6,200,000 PKR',  
+            color: ['red','white','black'],
+            image :'./brv.jpg'     
+        },
+        accord :{
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "15 to 19 KM/LITER",
+            Wheel :	'22 in',
+            price: '22,346,000 PKR',  
+            color: ['red','white','black'] ,
+            image :'./accord.jpg'    
+        },
+        civic :{
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "15 to 19 KM/LITER",
+            Wheel :	'18 in',
+            price: '8,329,000 PKR',  
+            color: ['red','white','black'] ,
+            image :'./cars.jpg'    
+        },
+        HRV :{
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "15 to 19 KM/LITER",
+            Wheel :	'16 in',
+            price: '7,649,000 PKR',  
+            color: ['red','white','black'] ,
+            image :'./cars.jpg'    
+        },
+        CRV :{
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "15 to 19 KM/LITER",
+            Wheel :	'14 in',
+            price: '8,658,000 PKR',  
+            color: ['red','white','black'],
+            image :'./cars.jpg'    
+
+        }
+
+
+    },
+    toyota : {
+        corolla :{
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "15 to 19 KM/LITER",
+            Wheel :	'16 in',
+            price: '5,960,000 PKR',  
+            color: ['red','white','black']   
+        },
+        yaris :{
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "15 to 19 KM/LITER",
+            Wheel :	'14 in',
+            price: '4,320,000 PKR',  
+            color: ['red','white','black']   
+        },
+        camry :{
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "15 to 19 KM/LITER",
+            Wheel :	'14 in',
+            price: '23,200,000 PKR',  
+            color: ['red','white','black']   
+        },
+        premio :{
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Automatic',
+            Mileage: "15 to 19 KM/LITER",
+            Wheel :	'14 in',
+            price: '13,200,000 PKR',  
+            color: ['red','white','black']   
+        },
+        landcruiser :{
+            model:'2023',
+            seatcapacity:'8 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "10 to 12 KM/LITER",
+            Wheel :	'19 in',
+            price: '23,200,000 PKR',  
+            color: ['red','white','black']   
+        },
+        crollacross :{
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "12 to 16 KM/LITER",
+            Wheel :	'16 in',
+            price: '9,399,000 PKR',  
+            color: ['red','white','black']   
+        },
+        landcruiser300 :{
+            model:'2023',
+            seatcapacity:'8 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "115 to 1 KM/LITER",
+            Wheel :	'18 in',
+            price: '33,200,000 PKR',  
+            color: ['red','white','black']   
+        },
+        fortuner :{
+            model:'2023',
+            seatcapacity:'8 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "15 to 19 KM/LITER",
+            Wheel :	'18 in',
+            price: '14,499,000 PKR',  
+            color: ['red','white','black'],   
+        },
+        revo :{
+            model:'2023',
+            seatcapacity:'5 person',
+            extra : 'loading area',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "10 to 13 KM/LITER",
+            Wheel :	'18 in',
+            price: '11,959,000 PKR',  
+            color: ['red','white','black'],   
+        },
+        hilux :{
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Auto',
+            Mileage: "15 to 19 KM/LITER",
+            Wheel :	'14 in',
+            price: '3,200,000 PKR',  
+            color: ['red','white','black'],   
+        },
+        hiace :{
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Automatic',
+            Mileage: "10 to 14 KM/LITER",
+            Wheel :	'14 in',
+            price: '13,450,000 PKR',  
+            color: ['red','white','black']   
+        }
+    },
+    hyundai :{
+        tucson :{
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Automatic',
+            Mileage: "10 to 14 KM/LITER",
+            Wheel :	'14 in',
+            price: '6,450,000 PKR',  
+            color: ['red','white','black']   
+        },
+        santafe : {
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Automatic',
+            Mileage: "10 to 14 KM/LITER",
+            Wheel :	'14 in',
+            price: '5,450,000 PKR',  
+            color: ['red','white','black']   
+        },
+        kona :{
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Automatic',
+            Mileage: "10 to 14 KM/LITER",
+            Wheel :	'18 in',
+            price: '7,870,000 PKR',  
+            color: ['red','white','black']   
+        },
+        palisade :{
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Automatic',
+            Mileage: "10 to 14 KM/LITER",
+            Wheel :	'16 in',
+            price: '8,670,000 PKR',  
+            color: ['red','white','black']   
+        },
+        venue :{
+            model:'2023',
+            seatcapacity:'5 person',
+            fuleType : 'petrol',
+            Transmission :'Type	Automatic',
+            Mileage: "10 to 14 KM/LITER",
+            Wheel :	'17 in',
+            price: '6,850,000 PKR',  
+            color: ['red','white','black']   
+        }
+        
+    }
+    
+}
+
+ 
+ var carShow =document.getElementById("carShow")
+ var select =document.getElementById("select")
+
+  function renderCard(){
+
+    // var elm =``
+ 
+    select.innerHTML =`<option value="">Select Brand Name </option>`
+    for(var category in carlist){
+        // console.log(category," iam category")
+        select.innerHTML +=` <option value="${category}">${category} </option>`
+
+        for(var model in carlist[category]){
+            // console.log(model,"me model huu")
+            // console.log(carlist[category][model] ,"dekho")
+
+            var card =carlist[category][model]
+
+            carShow.innerHTML +=`<div class="bg-purple-500 w-[30%] p-2 m-2 py-3 rounded-md shadow-xl hover:bg-purple-300 hover:scale-105">
+             <img src=${card.image ||'./cars.jpg' } class="w-[90%] p-2 rounded-xl ml-2  " alt="">
+            <p>Seatcapacity : ${card.seatcapacity} </p>
+            <p>FuleType : ${card.fuleType}</p>
+            <p>Transmission :${card.Transmission}</p>
+            <p>Wheel :${card.Wheel}</p>
+            <p class="font-mono font-semibold">Price : ${card.price}</p>
+            <p>Color ${card.color}</p>
+            <p>Milege :${card.Mileage}</p>
+
+        </div>`
+        }
+    }
+
+
+  }
+
+  renderCard()
+
+
+  var modelSelect =document.getElementById("modelSelect")
+
+
+   function selectBtn(){
+
+    // console.log( carlist.suzuki)
+    // console.log(select.value ,"me select change value huu")
+    let deko =select.value
+    // console.log(carlist[deko],"kuch aaya kya")
+    modelSelect.innerHTML =`<option value="">Select Model Name </option>`
+    carShow.innerHTML =''
+     for(var key in carlist[deko]){
+        // console.log(carlist[deko][key],"key me kya hai")
+        // console.log(key,"me kon huu")
+        modelSelect.innerHTML +=` <option value="${key}">${key} </option>`
+     
+        let colorDiv =''
+        
+
+     let card =carlist[deko][key];
+
+  carShow.innerHTML += `<div id="${key.toLowerCase()}" class="bg-purple-500 w-[30%] p-2 m-2 py-3 rounded-md shadow-xl hover:bg-purple-300 hover:scale-105 ${card.color}">
+             <img src="./car hundson copy.jpeg" class="w-[90%] p-2 rounded-xl ml-2  " alt="">
+             <p class="font-semibold">Name : ${key.toUpperCase()} (${card.model}) </p>
+             
+            <p>Seatcapacity : ${card.seatcapacity} </p>
+            <p>FuleType : ${card.fuleType}</p>
+            <p>Transmission :${card.Transmission}</p>
+            <p>Wheel :${card.Wheel}</p>
+            <p class="font-mono font-semibold">Price : ${card.price}</p>
+            <p>Color ${card.color}</p>
+            <p>Milege :${card.Mileage}</p>
+            
+
+        </div>`
+
+        }
+    
+   }
+
+
+    var carSelect =document.getElementById("key")
+    var singleCar =document.getElementById("singleCar")
+    
+    // one product aa gya hai 
+    
+    function modelSelectBtn(){
+         carShow.innerHTML =''
+        // console.log("key-->",key)
+
+        // console.log(carlist.suzuki.alto)
+        // console.log(select.value,modelSelect.value,"kuch aho")
+        // console.log(carlist[select.value][modelSelect.value],"one product")
+
+         let card =carlist[select.value][modelSelect.value]
+        let singleProduct = `<div class="card mb-3 bg-pink-400 flex flex-wrap gap-3 w-[80%]" >
+        <div class="row g-0 bg-purple-500">
+          <div class="col-md-4">
+            <img src="${card.image}" class="img-fluid rounded-start p-3 w-[100%]" alt="car">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Model: ${card.model}</h5>
+              <p class="card-text">Mileage :${card.Mileage}</p>
+              <p class="card-text">Transmission :${card.Transmission}</p>
+              <p class="card-text">Wheel :${card.Wheel}</p>
+              <p class="card-text">seatcapacity :${card.seatcapacity}</p>
+              <p class="card-text">color :${card.color}</p>
+              <p class="card-text"><small class="text-xl font-semibold">Price: $  ${card.price}</small></p>
+            </div>
+          </div>
+        </div>
+      </div>`
+
+      singleCar.innerHTML =singleProduct
+
+    }
+
+
+ 
+     function clearAll(){
+        singleCar.innerHTML =''
+        carShow.innerHTML =''
+       renderCard()
+
+
+     }
